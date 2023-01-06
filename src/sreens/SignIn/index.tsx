d05e77311f3e7e476151a5cd304c03499a6a54d4
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-06 17:25:06 -0300

    tela de splash

---
+++ src/sreens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from './styles';
+
+export function SignIn() {
+    return(
+        <View style={styles.container}>
+            <Text>
+                Ola, mundo
+            </Text>
+        </View>
+    )
+}
